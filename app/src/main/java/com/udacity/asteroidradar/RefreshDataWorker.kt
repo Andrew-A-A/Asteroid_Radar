1ae50be7c3aecabd7cdd1d33353a648292f4f568
Andrew Adel <andrew.adel.a@outlook.com>
2022-08-25 18:57:54 +0200

    The app now downloads the next 7 days asteroids and saves them in the database once a day using workManager with requirements of internet connection and device plugged in. The app can display saved asteroids from the database even if internet connection is not available

--- app/src/main/java/com/udacity/asteroidradar/RefreshDataWorker.kt
+++ app/src/main/java/com/udacity/asteroidradar/RefreshDataWorker.kt
@@ -1,4 +1,29 @@
 package com.udacity.asteroidradar
 
-class RefreshDataWorker {
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.Database.getDatabase
+import retrofit2.HttpException
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class RefreshDataWorker(appContext: Context,params: WorkerParameters):CoroutineWorker(appContext,params) {
+    companion object{
+        const val WORK_NAME="RefreshDataWorker"
+    }
+
+    private val current: LocalDateTime = LocalDateTime.now()
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern(Constants.API_QUERY_DATE_FORMAT)
+    private val startDate: String =current.format(formatter)
+    override suspend fun doWork(): Result {
+        val database= getDatabase(applicationContext)
+        val repository=DataRepository(database)
+        return try {
+            repository.refreshAsteroids(startDate)
+            Result.success()
+        }catch (e: HttpException){
+            Result.retry()
+        }
+    }
 }
